b304c022c84499055fd5a72c850023aaf14d6e61
Sunny Prakash <sunny.pprakash@gmail.com>
2021-09-27 14:12:33 +0530

    add route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
+	findPalette(id) {
+		return seedColors.find((palette) => {
+			return palette.id === id;
+		});
+	}
 	render() {
 		return (
-			<div>
+			<>
+				<Switch>
+					{/* <Route exact path="/" render={() => {}} /> */}
+					<Route
+						exact
+						path="/palette/:id"
+						render={(routeProps) => (
+							<Palette
+								palette={generatePalette(
+									this.findPalette(routeProps.match.params.id)
+								)}
+							/>
+						)}
+					/>
+				</Switch>
+				{/* <div>
 				<Palette palette={generatePalette(seedColors[4])} />
-			</div>
+			</div> */}
+			</>
 		);
 	}
 }
