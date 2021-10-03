38636fbb2ed80dd29445059684e7e244aafa0938
Sunny Prakash <sunny.pprakash@gmail.com>
2021-10-03 10:10:11 +0530

    Updates

---
+++ precache-manifest.ccdaaa9ed4f764026582f87f2aefb3d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f9db0413cd3c9972ca3",
+    "url": "/color-palette-app/static/css/main.74732aa6.chunk.css"
+  },
+  {
+    "revision": "3f9db0413cd3c9972ca3",
+    "url": "/color-palette-app/static/js/main.dace4383.chunk.js"
+  },
+  {
+    "revision": "77704f5ade45923a5191",
+    "url": "/color-palette-app/static/js/runtime~main.54b8cb8e.js"
+  },
+  {
+    "revision": "ee3b04909c496ffd8fb0",
+    "url": "/color-palette-app/static/css/2.5c191fe1.chunk.css"
+  },
+  {
+    "revision": "ee3b04909c496ffd8fb0",
+    "url": "/color-palette-app/static/js/2.59329aa3.chunk.js"
+  },
+  {
+    "revision": "a61690cbc3053823d5bf36df951ea884",
+    "url": "/color-palette-app/static/media/bg.a61690cb.svg"
+  },
+  {
+    "revision": "ae78de1109a0f4b81fdc260acf40d464",
+    "url": "/color-palette-app/index.html"
+  }
+];
